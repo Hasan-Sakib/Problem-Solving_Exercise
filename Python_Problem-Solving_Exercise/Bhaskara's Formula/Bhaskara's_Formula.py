501e5fb270ecd6dc4bfff7afaffb25bac0dd1750
Sakib <hasan.s4k1b@gmail.com>
2024-11-02 15:40:05 +0600

    Printing the roots of bhaskara’s formula

---
+++ Python_Problem-Solving_Exercise/Bhaskara's Formula/Bhaskara's_Formula.py
@@ -0,0 +1,11 @@
+a, b, c = list(map(float, input().split()))
+
+formula = b ** 2 - 4 * a * c
+
+if a == 0 or formula < 0:
+    print("Impossible calculation")
+else:
+    x1 = (-b + formula ** 0.50) / (2 * a)
+    x2 = (-b - formula ** 0.50) / (2 * a)
+    print(f'R1 = {x1:.5f}')
+    print(f'R2 = {x2:.5f}')
